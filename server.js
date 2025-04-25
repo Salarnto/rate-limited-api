f0ab7f7caf4bb437a45bd66fb0441afd60b1ee3c
Salarnto <salarnto.collab@gmail.com>
2025-04-25 11:07:15 +0330

    feat: add Redis connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import { connectToRedis } from './configs/redis.js';
 import rateLimiter from './middlewares/rateLimiter.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connections
+connectToRedis();
 
 // Middlewares
 app.use(express.json());
